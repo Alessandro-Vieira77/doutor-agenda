614cd95279046b5d9f231db107784fbc1106f19b
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-12-03 17:47:17 -0300

    feat: add clinic data to user session and render it on sideBar

--- src/app/(protected)/_components/AppSideBar.tsx
+++ src/app/(protected)/_components/AppSideBar.tsx
@@ -10,8 +10,10 @@ import {
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -30,6 +32,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { authClient } from "@/lib/auth-client";
 
 // Menu items.
 const items = [
@@ -56,7 +59,29 @@ const items = [
 ];
 
 export function AppSidebar() {
+  const router = useRouter();
   const pathName = usePathname();
+
+  const session = authClient.useSession();
+
+  const handleSignOut = async () => {
+    await authClient.signOut();
+    router.push("/authentication");
+  };
+
+  const handleAcronymName = (name: string) => {
+    return name
+      ?.split(" ")
+      ?.map((word, index) => {
+        if (index === 2) {
+          return;
+        }
+        return word[0];
+      })
+      ?.join("")
+      ?.toUpperCase();
+  };
+
   return (
     <Sidebar>
       <SidebarHeader className="py-6 pl-8">
@@ -78,7 +103,7 @@ export function AppSidebar() {
                   <SidebarMenuButton
                     asChild
                     isActive={pathName === item.url}
-                    className={`data-[active=true]:bg-sidebar-primary/10 data-[active=true]:text-sidebar-primary hover:bg-sidebar-primary/10 hover:text-sidebar-primary hover:font-semibold`}
+                    className={`data-[active=true]:text-sidebar-primary hover:text-sidebar-primary hover:font-semibold`}
                   >
                     <Link href={item.url}>
                       <item.icon
@@ -102,15 +127,19 @@ export function AppSidebar() {
                   <div className="flex items-center gap-2">
                     <Avatar className="h-10 w-10 rounded-lg">
                       <AvatarImage
-                        src="https://github.com/shadcn.png"
+                        src={session.data?.user?.image as string}
                         alt="image profile"
                       />
-                      <AvatarFallback>SC</AvatarFallback>
+                      <AvatarFallback>
+                        {handleAcronymName(session.data?.user?.name as string)}
+                      </AvatarFallback>
                     </Avatar>
                     <div className="flex flex-col gap-0.5">
-                      <p className="text-sm font-bold">Shadcn</p>
-                      <p className="text-muted-foreground text-xs">
-                        valessando33@gmail.com
+                      <p className="w-40 truncate text-sm font-bold">
+                        {session.data?.user?.clinic?.name}
+                      </p>
+                      <p className="text-muted-foreground w-40 truncate text-xs">
+                        {session.data?.user?.email}
                       </p>
                     </div>
                   </div>
@@ -122,8 +151,10 @@ export function AppSidebar() {
                 className="flex items-center gap-4"
               >
                 <DropdownMenuItem className="flex w-full items-center gap-4">
-                  <LogOut size={24} color="var(--primary)" />
-                  <span>logout</span>
+                  <Button variant="ghost" onClick={handleSignOut}>
+                    <LogOut size={24} color="var(--primary)" />
+                    <span>logout</span>
+                  </Button>
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
