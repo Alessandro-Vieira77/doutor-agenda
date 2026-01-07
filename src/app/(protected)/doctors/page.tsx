fd72a55033eaecc5c143b888cb1e85432f4325d7
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2026-01-07 14:40:20 -0300

    feat: responsive doctors

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -43,7 +43,7 @@ export default async function Doctors() {
         button={<AddDoctorButton />}
       />
       <ReusableContainerContent>
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
           {doctors?.map((doctor) => (
             <DoctorCard key={doctor.id} doctor={doctor} />
           ))}
